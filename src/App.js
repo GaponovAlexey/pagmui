a5befba519e29e8a4d3a8b1a4f4912fa69efd64a
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-12-02 22:11:15 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Link, Pagination, Stack, TextField } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-function App() {
+const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
+
+export default function App() {
+  const [posts, setPosts] = useState([])
+  const [query, setQury] = useState('react')
+  const [page, setPage] = useState(1)
+  const [pageQty, setPageQty] = useState(0)
+
+  useEffect(() => {
+    axios.get(BASE_URL + `page=${page - 1}&query=${query}`).then(({ data }) => {
+      console.log(data)
+      setPosts(data.hits)
+      setPageQty(data.nbPages)
+      if(data.nbPages < page) {
+        setPage(1)
+        
+      }
+    })
+  }, [page, query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container sx={{ marginTop: 1 }}>
+      <Stack spacing={1}>
+        <TextField
+          fullWidth
+          label='query'
+          value={query}
+          onChange={(e) => setQury(e.target.value)}
+        />
+        <Pagination
+          hideNextButton
+          hidePrevButton
+          page={page}
+          count={pageQty}
+          onChange={(_, str) => setPage(str)}
+        />
+        {posts.map((el) => (
+          <Link key={el.objectID} href={el.url}>
+            {el.title || el.story_title}
+          </Link>
+        ))}
+      </Stack>
+    </Container>
+  )
 }
-
-export default App;
