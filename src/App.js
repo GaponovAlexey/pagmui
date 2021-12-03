fa97c627d1aeec153ec28fcdc4ebd17f837cbe3b
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-12-03 21:45:47 +0300

    save

--- src/App.js
+++ src/App.js
@@ -5,13 +5,42 @@ import { useEffect, useState } from 'react'
 const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
 
 export default function App() {
-  let size = ['15px', '12pt', '34em', '-3px', '2%']
-  console.log(parseInt(size));
-  console.log(parseFloat(314e-2));
-  // function toNum(arr) {
-  //   for(let i = 0; i < arr.length; i++) {
-  //     arr[i] = 
-  //       }
-  // }
-  return <div>{size}</div>
+  const [pages, setPages] = useState([])
+  const [query, setQuery] = useState('react')
+  const [page, setPage] = useState(1)
+  const [quntity, setQuntity] = useState(0)
+
+  useEffect(() => {
+    axios.get(BASE_URL + `page=${page - 1}&query=${query}`).then(({ data }) => {
+      setPages(data.hits)
+      setQuntity(data.nbPages)
+    })
+  }, [page, query])
+
+  return (
+    <div>
+      <Container>
+        <TextField
+          fullWidth  
+          label='Search'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Pagination
+          page={page}
+          count={quntity}
+          onChange={(_, max) => setPage(max)}
+        />
+        <Stack>
+          {pages.map((el) => {
+            return (
+              <Link key={el.objectID} href={el.url}>
+                {el.title || el.story_text}
+              </Link>
+            )
+          })}
+        </Stack>
+      </Container>
+    </div>
+  )
 }
