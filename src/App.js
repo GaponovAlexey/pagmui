2bab361c96de1f46a26b67945a324eae9e3b429f
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-12-03 19:42:58 +0300

    save

--- src/App.js
+++ src/App.js
@@ -5,45 +5,13 @@ import { useEffect, useState } from 'react'
 const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
 
 export default function App() {
-  const [posts, setPosts] = useState([])
-  const [query, setQury] = useState('react')
-  const [page, setPage] = useState(1)
-  const [pageQty, setPageQty] = useState(0)
-
-  useEffect(() => {
-    axios.get(BASE_URL + `page=${page - 1}&query=${query}`).then(({ data }) => {
-      console.log(data)
-      setPosts(data.hits)
-      setPageQty(data.nbPages)
-      if(data.nbPages < page) {
-        setPage(1)
-        
-      }
-    })
-  }, [page, query])
-
-  return (
-    <Container sx={{ marginTop: 1 }}>
-      <Stack spacing={1}>
-        <TextField
-          fullWidth
-          label='query'
-          value={query}
-          onChange={(e) => setQury(e.target.value)}
-        />
-        <Pagination
-          hideNextButton
-          hidePrevButton
-          page={page}
-          count={pageQty}
-          onChange={(_, str) => setPage(str)}
-        />
-        {posts.map((el) => (
-          <Link key={el.objectID} href={el.url}>
-            {el.title || el.story_title}
-          </Link>
-        ))}
-      </Stack>
-    </Container>
-  )
+  let size = ['15px', '12pt', '34em', '-3px', '2%']
+  console.log(parseInt(size));
+  console.log(parseFloat(314e-2));
+  // function toNum(arr) {
+  //   for(let i = 0; i < arr.length; i++) {
+  //     arr[i] = 
+  //       }
+  // }
+  return <div>{size}</div>
 }
